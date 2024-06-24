daaeba06b0057e3fd8ed22e2c6339d200e3f96d7
Stefan Härtel <haertel.dev@gmail.com>
2024-06-24 18:24:10 +0200

    Hello world again!

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,25 @@ fn main() -> Result<()> {
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
 
-    // TODO main loop
+    loop {
+        terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(
+                Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+                    .white()
+                    .on_blue(),
+                area,
+            );
+        })?;
+        
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
 
     stdout().execute(LeaveAlternateScreen)?;
     disable_raw_mode()?;
